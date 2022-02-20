bf98626ab04108aaa69b784ee484ceed166e4443
JessicaLin01 <miley870101@gmail.com>
2022-02-21 03:23:02 +0800

    update&jquery ui

--- js/main15.js
+++ js/main15.js
@@ -140,3 +140,107 @@ function touchRead(){
         choose = 'story';
     }, false);
 }
+
+
+
+
+//update
+var Particle = function (x, y) {
+    var pixels = [];
+    this.sx = x; this.sy = y;
+    this.cx = x; this.cy = y;
+    this.dx = 0; this.dy = 0; // 手指頭目前點取的位置
+    this.velocity = 2.25 + Math.random() * 2; // 分子移動的速度
+    this.circlearound = false; // 進入繞圈圈模式
+    this.isMoving = false;
+    this.toFinger = false;
+    this.arc = 0;    // 目前分子跟水平方向的弧度(0,1)
+    this.speed = Math.round(Math.random() * 10) / 100;  // 旋轉的速度
+    this.clock = Math.random() *0.5 ;  // 個別計時器的起始時間
+    
+    if (Math.random() >= 0.5) this.dir = 1; // 旋轉的方向
+    else this.dir = -1;
+
+    this.dist = 1000 + Math.random() * 30; // 每一個分子距離手指頭位置距離平方
+    this.sqrtdist = Math.sqrt(this.dist); // 每一個分子距離手指頭位置距離平方
+
+    this.touchDown = function (tdx, tdy) {
+        this.dx = tdx; this.dy = tdy;
+        this.toFinger = true;
+    }
+    this.touchUp = function () {
+        this.toFinger = false;
+        this.circlearound = false;
+        // console.log("touchUp");
+    }
+    this.touchMove = function (tdx, tdy) {
+        this.dx = tdx; this.dy = tdy;
+    }
+    this.updateData = function () {
+        if (this.toFinger) { // 手指頭按下，分子向手指頭方向移動
+            if (!this.circlearound) {
+                var vx = this.dx - this.cx;
+                var vy = this.dy - this.cy;
+                if (Math.abs(vx) >= 0.01 || Math.abs(vy) >= 0.01) { // 兩者在一定的距離之外
+                    var t = Math.sqrt(vx * vx + vy * vy);
+                    vx = vx / t; vy = vy / t; // 單位的方向向量
+                    var nx = this.dx - (this.cx + vx * 0.5 * this.velocity); // 新的位置與觸控點的 x 向量
+                    var ny = this.dy - (this.cy + vy * 0.5 * this.velocity); // 新的位置與觸控點的 y 向量
+                    this.cx = this.dx - nx;
+                    this.cy = this.dy - ny;
+                    if ((nx * nx + ny * ny) >= this.dist) { // 未達繞圈圈的距離,更新座標
+                        this.circlearound = false;
+                    }
+                    else { // 進入繞圈圈狀態
+                        // 計算目前所在的角度
+                        this.arc = Math.acos(vy);
+                        this.circlearound = true;
+                    }
+                }
+            }
+            else { // 進入繞圈圈狀態
+                // 計算兩者距離是否還在繞圈圈的範圍內
+                var vx = this.dx - this.cx;
+                var vy = this.dy - this.cy;
+                if ( (vx * vx + vy * vy) > (this.dist+5) ) { // 不再距離內
+                    var t = Math.sqrt(vx * vx + vy * vy);
+                    vx = vx / t; vy = vy / t; // 單位的方向向量
+                    var nx = this.dx - (this.cx + vx * 0.5 * this.velocity); // 新的位置與觸控點的 x 向量
+                    var ny = this.dy - (this.cy + vy * 0.5 * this.velocity); // 新的位置與觸控點的 y 向量
+                    this.circlearound = false;
+                }
+                else { // 繞圈圈的距離內
+                    this.arc = this.arc + this.dir*this.speed;
+                    this.cx = this.dx + this.sqrtdist * Math.cos(this.arc);
+                    this.cy = this.dy + this.sqrtdist * Math.sin(this.arc);
+                }
+            }
+            this.isMoving = true; // 代表分子目前正在移動中
+        }
+        else { // 
+            if (this.isMoving) { 
+                // 代表分子之前是在移動中，現在要移動回原來的位置，當到達預設位置時，將 this.isMoving 設定為 false
+                this.cx = this.cx + (this.sx - this.cx) * 0.025 *this.velocity;
+                this.cy = this.cy + (this.sy - this.cy) * 0.025 * this.velocity;
+
+                if (Math.abs(this.cx - this.sx) <= 0.1 && Math.abs(this.cy - this.sy) <= 0.1) {
+                    this.isMoving = false;
+                    this.cx = this.sx; this.cy = this.sy;
+                }
+            }
+        }
+
+        this.clock = this.clock + 0.0135;
+        this.drawC();
+    }
+    this.drawC = function () {
+        // 根據 this.clock 計算透明度的變化
+        if (this.clock >= 3.1415) this.clock = this.clock - 3.1415;
+        var opacity = Math.floor(64 + 191 * Math.abs(Math.sin(this.clock)));
+        ctx.beginPath();
+        ctx.arc(this.cx, this.cy, 5, 0, 360, false);
+        ctx.fillStyle = "#F8F9EC" + opacity.toString(16);
+        ctx.fill();
+        ctx.closePath();
+    };
+}
